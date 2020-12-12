c99be5293a3eb550068c8677f66d33bd0dd491ca
Tony Spiro <tspiro@tonyspiro.com>
2020-12-11 19:53:16 -0600

    fix imgix component issue

--- components/cover-image.js
+++ components/cover-image.js
@@ -1,7 +1,6 @@
 import cn from 'classnames'
 import Link from 'next/link'
 import Imgix from 'react-imgix'
-import { getMergeId } from '@/lib/merge'
 
 export default function CoverImage({ title, url, slug }) {
   let image = (
@@ -12,24 +11,11 @@ export default function CoverImage({ title, url, slug }) {
         'hover:shadow-medium transition-shadow duration-200': slug,
       })}
       sizes="100vw"
-      attributeConfig={{
-        src: 'data-src',
-        srcSet: 'data-srcset',
-        sizes: 'data-sizes',
-      }}
       htmlAttributes={{
         src: `${url}?auto=format,compress&q=1&blur=500&w=auto`,
       }}
     />
   )
-  // Because Imgix component weirdness
-  const merge_id = getMergeId()
-  if (merge_id)
-    image = (
-      <Link as={`/posts/${slug}`} href="/posts/[slug]">
-        <a><img className="w-full" title={title} src={`${url}?w=1000`} slug={slug} /></a>
-      </Link>
-    )
   return (
     <div className="sm:mx-0">
       {slug ? (
